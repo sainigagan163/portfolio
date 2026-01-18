762ce67eba5a2b8f4eee778ce959a6fdd4d02489
sainigagan163 <sainigagan163@gmail.com>
2026-01-18 22:44:24 +0000

    Add optional GA4 analytics initialization

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+declare global {
+  interface Window {
+    dataLayer?: unknown[];
+  }
+}
+
+const initializeAnalytics = () => {
+  const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+  if (!measurementId) {
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer ?? [];
+  const gtag = (...args: unknown[]) => {
+    window.dataLayer?.push(args);
+  };
+
+  gtag('js', new Date());
+  gtag('config', measurementId, { anonymize_ip: true });
+};
+
+initializeAnalytics();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
